6bf307ee0a815489ef9e8516bdbae72a71cc1034
the Tap <97515458+Tapicka1984@users.noreply.github.com>
2022-11-27 16:14:01 +0100

    Add files via upload

---
+++ WindowsFormsApp9/Clovek.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp9
+{
+    class Clovek
+    {
+        private string T_jmeno;
+        DateTime T_narozen;
+
+        public Clovek(string jmeno, DateTime roky)
+        {
+            this.T_jmeno = jmeno;
+            this.T_narozen = roky;
+        }
+
+        public void Vypis_jmeno()
+        {
+            MessageBox.Show("jmeno je " + T_jmeno);
+        }
+
+        TimeSpan Vek()
+        {
+            TimeSpan vek = DateTime.Now - T_narozen;
+            return vek;
+        }
+
+        bool Plnolety()
+        {
+            TimeSpan vek = Vek();
+            if(vek.Days/365.25 >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            bool ano_ne = Plnolety();
+            DateTime ted = DateTime.Now;
+            TimeSpan roky = ted - T_narozen;
+            if (ano_ne == true)
+            {
+                MessageBox.Show(T_jmeno + " je stary " + roky.Days/365.25 + " let a je plnolety");
+                return null;
+            }
+            else
+            {
+                MessageBox.Show(T_jmeno + " je stary" + roky.Days/365.25 + " let a neni plnolety");
+                return null;
+            }
+        }
+
+        public void Starsi(string jmeno2, DateTime roky2)
+        {
+            if (T_narozen < roky2)
+            {
+                MessageBox.Show(T_jmeno + " je starsi");
+            }
+            else if (T_narozen == roky2)
+            {
+                MessageBox.Show("Oba jsou stejne stari");
+            }
+            else
+            {
+                MessageBox.Show(jmeno2 + " je starsi typek");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name_1;
+            DateTime vek_1 = DateTime.Now;
+            name_1 = textBox1.Text;
+            if (textBox2.Text != string.Empty)
+            {
+                vek_1 = DateTime.Parse(textBox2.Text);
+            }
+
+            string name_2;
+            DateTime vek_2 = DateTime.Now;
+            name_2 = textBox3.Text;
+            if(textBox4.Text != string.Empty)
+            {
+                vek_2 = DateTime.Parse(textBox4.Text);
+            }
+
+            Clovek clovek = new Clovek(name_1, vek_1);
+            clovek.Vypis_jmeno();
+            clovek.ToString();
+            clovek.Starsi(name_2, vek_2);
+        }
+    }
+}
